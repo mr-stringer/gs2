3aa332b5662aa5438b0a8e2bd6258ab35ec8bf1f
Steve Stringer <sstringer@int.centiq.co.uk>
2020-07-17 17:25:38 +0100

    Added new parameter and tests

--- main_test.go
+++ main_test.go
@@ -1,13 +1,6 @@
 package main
 
-import (
-	"io/ioutil"
-	"log"
-	"os"
-	"testing"
-)
-
-func TestMain(m *testing.M) {
-	log.SetOutput(ioutil.Discard)
-	os.Exit(m.Run())
-}
+//func TestMain(m *testing.M) {
+//	log.SetOutput(ioutil.Discard)
+//	os.Exit(m.Run())
+//}
